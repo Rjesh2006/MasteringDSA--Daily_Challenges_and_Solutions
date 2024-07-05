a070351014349726e18fd1425d357f00d98c5220
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2024-07-05 12:39:13 +0530

    Create third.cpp

---
+++ day2/third.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Enter the n:";
+    cin>>n;
+    int i=1;
+
+
+    while(i<=n){
+        int j=1;
+
+
+        while(j<=(n-i+1)){
+            cout<<"*"<<n-i+1;
+
+            j+=1;
+
+
+        }
+          cout<<"\n";
+    i+=1;
+    }
+    
+
+    return 0;
+}
