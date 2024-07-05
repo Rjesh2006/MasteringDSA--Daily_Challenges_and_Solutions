0ebc577fc0380c5e6c4c3c4e099d00cdcd4a082d
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2024-07-05 12:47:46 +0530

    Update first_p.cpp

--- day5/first_p.cpp
+++ day5/first_p.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+#include <math.h>
 
+
+int main (){
+    long long n ;
+    cout <<"Enter n here which u want to convert in binary digit's:";
+    cin>>n;
+    cout<<endl;
+      int ans=0;
+          int i =0;
+        cout <<"Ans is initialized by :"<<ans<<endl;
+
+
+
+    while(n!=0){
+        int bit=n&1;
+        ans = (bit*pow(10 ,i))+ans;
+         
+        cout<<"incrimeted :"<<" "<<ans<<endl;
+
+        n= n>>1;
+        i++;
+
+
+    };
+    cout<<endl;
+    cout<<"Your number is converted in biinary digit's :"<<ans<<endl;
+}
