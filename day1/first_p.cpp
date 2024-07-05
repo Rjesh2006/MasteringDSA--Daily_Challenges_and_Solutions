0d578aba63e0f8504a3edd5acda81c8f42ee70dd
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2024-07-05 12:31:40 +0530

    Update first_p.cpp

--- day1/first_p.cpp
+++ day1/first_p.cpp
@@ -1,1 +1,26 @@
 
+# include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<" enter the number of n:"<<"\n";
+    cin>>n;
+    int i=0;
+    int total=0;
+
+    while (i<n){
+       
+        total =total +i;
+        i += 2;
+        cout<<"value of i:"<<i<<"\n";//here we updated the value of the i by +2 so tht we wwant the sum of the even numbers 
+    
+
+
+    };        
+
+    cout <<"the total is :"<<total;
+
+    return 0;
+
+
+}
