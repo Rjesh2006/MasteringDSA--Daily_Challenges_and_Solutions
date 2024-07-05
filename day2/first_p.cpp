57e50d5c74d58a0ee5b028da7ae7cdaa820d4ea6
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2024-07-05 12:38:08 +0530

    Update first_p.cpp

--- day2/first_p.cpp
+++ day2/first_p.cpp
@@ -1,1 +1,21 @@
+// character pattern
+#include<iostream>
+using namespace std;
+int main (){
+    int n=4;
+    cout<<"\n";
+    int row=1;
+    while (row<=n){
+      int col=1;
+        while(col<=n){
+          char ch='A'+row-1;
+          cout<<(ch);
+          col+=1;
 
+    }
+    cout<<"\n";
+     row+=1;
+    }
+    
+    return 0;
+}
